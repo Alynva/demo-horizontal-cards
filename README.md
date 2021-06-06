28956838879664d290ff54e5343cbba1c36685be
Alisson Nunes <alynva@gmail.com>
2021-06-05 23:32:12 -0300

    First version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # demo-horizontal-cards
+
+Cloned from the amazing video:
+- Thinking on ways to solve a MEDIA SCROLLER
+- By Google Chrome Developers
+- https://www.youtube.com/watch?v=jmLdZY_Lo1k
+
+## Articles
+
+- What does it take to be installable? https://web.dev/install-criteria/
+- What makes a good Progressive Web App? https://web.dev/pwa-checklist/
+- Workbox: your high-level service worker toolkit https://web.dev/workbox/
---
+++ index.js
@@ -0,0 +1,26 @@
+// Enable use the arrow keys to navigate the focus and use tab to skip to the
+// next section. Without it, tab will focus each element in the list.
+document.querySelectorAll('.horizontal-media-scroller')
+	.forEach(scroller => rovingUx.rovingIndex({
+		element: scroller,
+		target: 'a',
+	}))
+
+// register service worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/service-worker.js')
+		.then(function (reg) {
+
+			if(reg.installing) {
+			console.log('Service worker installing');
+			} else if(reg.waiting) {
+			console.log('Service worker installed');
+			} else if(reg.active) {
+			console.log('Service worker active');
+			}
+
+		}).catch(function(error) {
+			// registration failed
+			console.log('Registration failed with ' + error);
+		});
+}
